fa5efaa953996ac2a042095398c2b3712bf44787
ryo273917 <ttc2350sg0009@edu.tech.ac.jp>
2026-09-05 18:36:10 +0900

    a

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    // プレイヤーの得点
+    private int player1Score = 0;
+    private int player2Score = 0;
+
+    // UI
+    public TMP_Text player1ScoreText;
+    public TMP_Text player2ScoreText;
+    public TMP_Text messageText;
+    public TMP_Text pointEffectText;
+
+    void Start()
+    {
+        UpdateScoreUI();
+
+        messageText.text = "";
+
+        // +1表示を最初は非表示
+        pointEffectText.gameObject.SetActive(false);
+    }
+
+    // PLAYER 1がペアを取った
+    public void Player1GetPair()
+    {
+        player1Score++;
+
+        UpdateScoreUI();
+
+        messageText.text = "PLAYER 1 +1 POINT!";
+
+        pointEffectText.text = "+1 POINT!";
+        pointEffectText.gameObject.SetActive(true);
+    }
+
+    // PLAYER 2がペアを取った
+    public void Player2GetPair()
+    {
+        player2Score++;
+
+        UpdateScoreUI();
+
+        messageText.text = "PLAYER 2 +1 POINT!";
+
+        pointEffectText.text = "+1 POINT!";
+        pointEffectText.gameObject.SetActive(true);
+    }
+
+    // UIのポイントを更新
+    void UpdateScoreUI()
+    {
+        player1ScoreText.text = "★ " + player1Score + " POINT";
+        player2ScoreText.text = "★ " + player2Score + " POINT";
+    }
+}
